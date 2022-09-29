839cfcef3428d55990d009721cdbf6480631aae8
Leprekus <leprekus@yahoo.com>
2022-09-28 22:15:17 -0700

    test implementation for empty string and no match

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,7 +1,8 @@
 function filterByTerm (input, str){
-    const filtered = input.filter(x => x.url.toLowerCase().includes(str.toLowerCase()))
-    return filtered;
-};
+    const filtered = str === '' ? 'No Search Term Provided' :
+    input.filter(x => x.url.toLowerCase().includes(str.toLowerCase()))
+    return filtered.length ? filtered : 'No Match'
+}
 
 describe('Filter function', () => {
     test('It should filter by search term \'link\'', () => {
@@ -15,5 +16,23 @@ describe('Filter function', () => {
 
         expect(filterByTerm(input, 'LINK')).toEqual(output)
 
+    });
+    test('It should return "No search Term Provided" with empty string', () => {
+        const input = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+            { id: 3, url: "https://www.link3.dev" }      
+        ]
+        const output = 'No Search Term Provided'
+        expect(filterByTerm(input, '')).toEqual(output)
+    });
+    test('It should return "No Match" with empty search term', () => {
+        const input = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+            { id: 3, url: "https://www.link3.dev" }      
+        ]
+        const output = 'No Match'
+        expect(filterByTerm(input, 'a')).toEqual(output)
     });
 });
